dbbec229076b74036175ad1c0b26bffb96c9182d
Tyuiodragon <tylertolbert@yahoo.com>
2018-03-03 22:02:30 -0600

    added waypoints

---
+++ js/waypoint.js
@@ -0,0 +1,73 @@
+var $showcaseTitle = $('#showcase .content-title');
+var $showcase = $('#showcase .wrapper');
+var $A = $('#section-a .content-wrap');
+var $Bpic = $('#section-b .p2');
+var $Btext = $('#section-b .content-title');
+var $C = $('#section-c .content-wrap');
+var $D = $('#section-d .content-wrap');
+var $Epic = $('#section-e .p9');
+var $Ewrap = $('#section-e .wrapper, #section-e .wrapper-2');
+var $F = $('#section-f .content-wrap');
+var $G = $('#section-g .content-wrap');
+var $Gpic = $('#section-g .p13');
+
+
+// The waypoints
+
+$showcaseTitle.waypoint(function () {
+	$showcaseTitle.addClass('js-show fadeInLeft animated');
+}, { offset: '60%'});
+
+$showcase.waypoint(function () {
+	$showcase.addClass('js-show fadeInRightBig animated');
+}, { offset: '60%'});
+
+$A.waypoint(function () {
+	$A.addClass('js-show fadeInUp animated');
+}, {offset: '60%'});
+
+$Bpic.waypoint(function () {
+	$Bpic.addClass('js-show fadeInUp animated');
+}, {offset: '60%'});
+
+$Btext.waypoint(function () {
+	$Btext.addClass('js-show fadeInLeft animated');
+}, {offset: '90%'});
+
+$C.waypoint(function () {
+	$C.addClass('js-show fadeInUp animated');
+}, {offset: '40%'});
+
+$D.waypoint(function () {
+	$D.addClass('js-show fadeInUp animated');
+}, {offset: '40%'});
+
+$Epic.waypoint(function () {
+	$Epic.addClass('js-show fadeInUp animated');
+}, {offset: '45%'});
+
+$Ewrap.waypoint(function () {
+	$Ewrap.addClass('js-show fadeInRight animated');
+}, {offset: '60%'});
+
+$F.waypoint(function () {
+	$F.addClass('js-show fadeInUp animated');
+}, {offset: '40%'});
+
+$G.waypoint(function () {
+	$G.addClass('js-show fadeInLeft animated');
+}, {offset: '60%'});
+
+$Gpic.waypoint(function () {
+	$Gpic.addClass('js-show fadeInRight animated');
+}, {offset: '60%'});
+
+
+
+
+
+
+
+
+
+
